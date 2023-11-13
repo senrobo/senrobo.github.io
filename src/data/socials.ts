96ebe477416f10da2814fb60cc2f92a92ae35d30
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-11-13 22:41:51 +0800

    Rewrote in Astro

---
+++ src/data/socials.ts
@@ -0,0 +1,10 @@
+export const author = "Sentinels Robotics";
+
+export const defaultMeta = {
+  title: "Sentinels Robotics",
+  description: "The best way to learn is to watch your robot go up in flames.",
+  ogImage: {
+    src: "/static/banner.png",
+    alt: "Sentinels Robotics",
+  },
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        hero: "url('../images/hero.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
