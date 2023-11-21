5b240e58c073c58322baa6a5f3ea7dddd0e4d8c7
Vincent Cayadi <vincentcayadi45@gmail.com>
2023-11-22 00:17:36 +0800

    removed junk code

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -72,24 +72,6 @@ export const getOgImagePath = (filename: string = defaultMeta.title) => {
  * @param text
  */
 
-const fetchFonts = async () => {
-  // Regular Font
-  const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf",
-  );
-  const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
-
-  // Bold Font
-  const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf",
-  );
-  const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
-
-  return { fontRegular, fontBold };
-};
-
-const { fontRegular, fontBold } = await fetchFonts();
-
 const generateOgImage = async (
   text: string = defaultMeta.title,
   date: Date = new Date(),
